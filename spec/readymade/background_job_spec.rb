4e70b98b4f26b3f477fe20b92720c61ab7f89470
AndriyAndriyovuch <andriy.mazurok.tl@gmail.com>
2025-09-20 15:58:48 +0300

    Added basic test

--- spec/readymade/background_job_spec.rb
+++ spec/readymade/background_job_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'byebug'
 RSpec.describe Readymade::BackgroundJob do
   class Dummy < Readymade::Action
   end
@@ -45,6 +46,51 @@ RSpec.describe Readymade::BackgroundJob do
           expect(res.queue_name).to eq('test')
           expect(res.arguments.first[:job_options]).to eq(job_options)
         end
+      end
+    end
+
+    context 'when lock_jobs is true' do
+      before do
+        allow(Readymade).to receive(:config).and_return(Readymade::Config.new)
+        Readymade.config.lock_jobs = true
+        Readymade.config.lock_ttl = 3.seconds
+        Readymade::BackgroundJob.apply_uniqueness!
+        ActiveJob::Uniqueness.unlock!
+      end
+
+      it 'creates instance variables from arguments' do
+        allow_any_instance_of(Dummy).to receive(:call_async).with(**args)
+
+        res = described_class.perform_later(**args.merge!(class_name: dummy_class.name))
+        res2 = described_class.perform_later(**args.merge!(class_name: dummy_class.name))
+
+        expect(res.job_id.size).to eq(36)
+        expect(res.queue_name).to eq('default')
+
+        expect(res2).to be_falsey
+      end
+    end
+
+    context 'when lock_jobs is false' do
+      before do
+        allow(Readymade).to receive(:config).and_return(Readymade::Config.new)
+        Readymade.config.lock_jobs = false
+        Readymade.config.lock_ttl = 3.seconds
+        Readymade::BackgroundJob.apply_uniqueness!
+        ActiveJob::Uniqueness.unlock!
+      end
+
+      it 'creates instance variables from arguments' do
+        allow_any_instance_of(Dummy).to receive(:call_async).with(**args)
+
+        res3 = described_class.perform_later(**args.merge!(class_name: dummy_class.name))
+        res4 = described_class.perform_later(**args.merge!(class_name: dummy_class.name))
+
+        expect(res3.job_id.size).to eq(36)
+        expect(res3.queue_name).to eq('default')
+
+        expect(res4.job_id.size).to eq(36)
+        expect(res4.queue_name).to eq('default')
       end
     end
   end
